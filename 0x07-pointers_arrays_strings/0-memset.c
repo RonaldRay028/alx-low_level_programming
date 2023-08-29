95a1ce8e34f79ae4711a2948627ff39c8843b5d5
RonaldRay028 <lovedaympofu2001@outlook.com>
2023-08-29 13:47:22 -0700

    fixed

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,42 +1,19 @@
-#include "main.h"
-#include <stdio.h>
+#include "holberton.h"
 /**
- *_memset  fills a block of memory with a specific value
- *starting address of memory to be filled
- *the desired value
- *number of bytes to be changed
- *Return: changed array with new value for n bytes
+ * _memset - function that fills memory with a constant byte
+ *@s: first value
+ *@b: second value
+ *@n: thirth value
+ *
+ * Return: char with result of memset
  */
-void simple_print_buffer( char *buffer, unsigned int size)
+char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i ;
-	i = 0 ;
-	while (i < size)
-	{
-		if (i % 10)
-		{
-			printf(" ");
-		}
-		if (!(i % 10) && i )
-		{
-			printf("\n);
-		}
-		printf("0x%02x" , buffer[i]);
-		i++;
-	}
-	printf("\n");
-}
-/**
- * main - check teh code 
- * return: Always 0.
- */
-int main(void )
-{
-	char buffer[98] = { 0x00};
-	simple_print_buffer(buffer, 98)
-	_memset(buffer, 0x01 ,95);
-	printf("----------------------------------------------\n")
-	simple_print_buffer(buffer, 98);
-	return(0);
-}
+	unsigned int i;
 
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
